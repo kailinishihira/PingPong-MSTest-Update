38d792b92571f1e6322f122ced2cdbbe90332a65
Kaili Nishihira and Robert Murray <robert@>
2017-08-07 12:10:57 -0700

    Add cshtml

--- PingPong/Controllers/HomeControllers.cs
+++ PingPong/Controllers/HomeControllers.cs
@@ -7,8 +7,16 @@ namespace PingPong.Controllers
 {
     public class HomeController : Controller
     {
-
-
-
+      [HttpGet("/")]
+      public ActionResult Index()
+      {
+        return View();
+      }
+      [HttpPost("/pingpong/results")]
+      public ActionResult Results()
+      {
+        PingPongGenerator pingpong = new PingPongGenerator (int.Parse(Request.Form["inputNumber"]));
+        return View (pingpong.GetList());
+      }
     }
 }
